d8b7a56b711d4d1f843db2b21848d595299ab8ab
Austin Donlevy <adonlevy2@gmail.com>
2021-05-28 07:43:50 -0400

    updates

---
+++ TV Show Search App/app.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('#SearchForm')
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const searchTerm = this.elements.query.value;
+    //Make API call
+    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`);
+    //Remember to execute your function and actually pass data in. I forgot at first
+    makeImages(res.data);
+})
+
+//This function gets the image url from the API response and appends it to the body of our document
+const makeImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img)
+        }
+    }
+}
